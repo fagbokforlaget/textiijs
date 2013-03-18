3cac381b9a3db34bc261374f271cbb9b222e04c4
Rafal Wrzochol <rafal@wrzochol.net>
2013-03-18 20:44:27 +0100

    read text directly rather than reading from file

--- test/textii.test.js
+++ test/textii.test.js
@@ -1,5 +1,8 @@
 var assert = require("assert"),
-    textii = require('../index');
+    textii = require('../index'),
+    sample_text = "Zero, one and three or five, six, seven... seven...\n" +
+                  "1,2,3... a,b,c... should not be INCLUDED:)\n" +
+                  "One more test (testing id good) - always!"
 
 describe('textii', function() {
 
@@ -8,7 +11,7 @@ describe('textii', function() {
     describe('when NO options given', function() {
 
       it('should set default options', function() {
-        var tii = new textii("example1.txt", null, function() {});
+        var tii = new textii(sample_text, null, function() {});
         assert.equal(tii.default_options, tii.options);
       });
 
@@ -18,7 +21,7 @@ describe('textii', function() {
 
       it('should set merged options', function() {
         var opts = { "min_word_length": 1, "something": "else" },
-            tii = new textii("example1.txt", opts, function() {});
+            tii = new textii(sample_text, opts, function() {});
         assert.equal(1, tii.options.min_word_length);
         assert.equal(tii.default_options.word_separator, tii.options.word_separator);
         assert.equal("else", tii.options.something);
@@ -33,19 +36,19 @@ describe('textii', function() {
     describe('section name NOT given', function() {
 
       it('should create reverted index without sections', function(done) {
-        var tii = new textii("test/txt/sample1.txt", null, function(err, data) {
-              if (err) {
-                throw err;
-              }
-              var expected = {
-                "zero":[0], "three":[3], "five":[5], "six":[6], "seven":[7,8],
-                "included":[18],
-                "one":[19], "test":[21,22], "good":[24], "alwai":[25]
-              };
-              assert(expected.equals(data));
+        var tii = new textii(sample_text, null, function(err, data) {
+          if (err) {
+            throw err;
+          }
+          var expected = {
+            "zero":[0], "three":[3], "five":[5], "six":[6], "seven":[7,8],
+            "included":[18],
+            "one":[19], "test":[21,22], "good":[24], "alwai":[25]
+          };
+          assert(expected.equals(data));
 
-              done();
-            });
+          done();
+        });
         tii.get();
       });
 
@@ -54,19 +57,19 @@ describe('textii', function() {
     describe('section name given', function() {
 
       it('should create reverted index with section', function(done) {
-        var tii = new textii("test/txt/sample1.txt", null, function(err, data) {
-              if (err) {
-                throw err;
-              }
-              var expected = {
-                "zero":{"page":[0]}, "three":{"page":[3]}, "five":{"page":[5]}, "six":{"page":[6]}, "seven":{"page":[7,8]},
-                "included":{"page":[18]},
-                "one":{"page":[19]}, "test":{"page":[21,22]}, "good":{"page":[24]}, "alwai":{"page":[25]}
-              };
-              assert(expected.equals(data));
-              done();
-            }),
-            get_opts = {"section": "page"};
+        var tii = new textii(sample_text, null, function(err, data) {
+          if (err) {
+            throw err;
+          }
+          var expected = {
+            "zero":{"page":[0]}, "three":{"page":[3]}, "five":{"page":[5]}, "six":{"page":[6]}, "seven":{"page":[7,8]},
+            "included":{"page":[18]},
+            "one":{"page":[19]}, "test":{"page":[21,22]}, "good":{"page":[24]}, "alwai":{"page":[25]}
+          };
+          assert(expected.equals(data));
+          done();
+        }),
+        get_opts = {"section": "page"};
         tii.get(get_opts);
       });
 

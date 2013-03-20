719f34a98b74ccd6192caa25575a0d6e6cd4a34b
Rafal Wrzochol <rafal.wrzochol@fagbokforlaget.no>
2013-03-20 09:06:09 +0100

    make examples runnable from within package source

--- examples/noopts1.js
+++ examples/noopts1.js
@@ -1,4 +1,5 @@
-var textii = require('textiijs'),
+// var textii = require('textiijs'),
+var textii = require('../index'),
     sample_text = "Zero, one and three or five, six, seven... seven...";
 
 var pii = new textii(sample_text, null, function(err, data) {
--- examples/opts1.js
+++ examples/opts1.js
@@ -1,4 +1,5 @@
-var textii = require('textiijs'),
+// var textii = require('textiijs'),
+var textii = require('../index'),
     sample_text = "Zero, one and three or five, six, seven... seven...",
     options = { "word_separator": /\W+/, "min_word_length": 3, "encoding": "utf8" },
     get_options = { "section": "page1" };
